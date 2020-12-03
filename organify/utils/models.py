e182a926b69d0d98374022d66fc714b91f5ec4e9
Xavier Medina <xmedinavei@gmail.com>
2020-12-03 13:08:57 -0500

    utils created

---
+++ organify/utils/models.py
@@ -0,0 +1,23 @@
+'''Project models utilities.'''
+
+# Django
+from django.db import models
+
+class OrganifyBaseModel(models.Model):
+    '''Organify base abstract model.
+    This will be the base that other project models will inherit. 
+    They will inherit the created (DateTime) and modified (DateTime) attributes.
+    '''
+
+    created = models.DateTimeField(auto_now_add=True,
+                                   help_text='Object datetime when created.')
+
+    modified = models.DateTimeField(auto_now=True,
+                                    help_text='Object datetime when modified.')
+
+    class Meta:
+        '''Meta options.'''
+
+        abstract = True
+        get_latest_by = 'created'
+        ordering = ['-created', '-modified']
