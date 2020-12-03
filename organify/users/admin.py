9ad532fcf78a1a9e958e02ab727626baddbb52a5
Xavier Medina <xmedinavei@gmail.com>
2020-12-03 17:10:16 -0500

    Repairing User Custom model

--- organify/users/admin.py
+++ organify/users/admin.py
@@ -6,4 +6,13 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
-admin.site.register(User, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    '''Customer User model admin.'''
+
+    list_display = ('id','email', 'username', 'first_name',
+                    'last_name', 'password', 'is_active')
+    list_filter = ('is_active',)
+    ordering = ['-id']
+
+admin.site.register(User, CustomUserAdmin)
--- organify/users/models/users.py
+++ organify/users/models/users.py
@@ -4,11 +4,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Utilities
-from utils.models import OrganifyBaseModel
 
-
-class User(OrganifyBaseModel, AbstractUser):
+class User(AbstractUser):
     '''User model.
     Extends from OrganifyBaseModel and Django AbstractUser.
     We add the email field here and put it as username field.
@@ -16,14 +13,24 @@ class User(OrganifyBaseModel, AbstractUser):
     
     email = models.EmailField(unique=True,
                               error_messages={'unique': 'This email already exists.'})
+    created = models.DateTimeField(auto_now_add=True,
+                                   help_text='Object datetime when created.')
+    modified = models.DateTimeField(auto_now=True,
+                                    help_text='Object datetime when modified.')
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
+    class Meta:
+        '''Meta options.'''
+       
+        get_latest_by = 'created'
+        ordering = ['-created', '-modified']
+
     def __str__(self):
         '''Return id, email, first_name and last_name.'''
 
-        to_show = f'{self.id}, {self.username}, {self.first_name} {self.last_name}'
+        to_show = f'{self.id}, {self.email}, {self.username}, {self.first_name} {self.last_name}'
         return to_show
 
     def get_short_name(self):
