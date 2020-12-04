299c16fedec77725bcf902a8f73146df131da32d
Xavier Medina <xmedinavei@gmail.com>
2020-12-04 14:13:13 -0500

    MOdify users

--- organify/users/views.py
+++ organify/users/views.py
@@ -1,4 +1,4 @@
-'''Users views'''
+'''Users viewsets'''
 
 # Django REST Framework
 from rest_framework import status, viewsets
@@ -20,8 +20,9 @@ from .serializers import (
 from .models import User
 
 class UserViewSet(viewsets.ModelViewSet):
-    '''User model view set. 
-    For sign up, login.
+    '''
+    User model view set. 
+    For sign up, login and delete account.
     '''
 
     queryset = User.objects.filter(is_active=True)
@@ -32,7 +33,6 @@ class UserViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'])
     def signup(self, request):
         '''User sign up view using the UserSignUpSerializer.'''
-
         serializer = UserSignUpSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
@@ -44,12 +44,25 @@ class UserViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['post'])
     def login(self, request):
         '''User login.'''
-
         serializer = UserLoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user, token = serializer.save()
         data = {
             'user': UserModelSerializer(user).data,
             'access_token': token,
+        }
+        return Response(data, status=status.HTTP_200_OK)
+
+    # URL: users/delete/
+    @action(detail=False, methods=['delete'])
+    def delete(self, request):
+        '''Inactive user account'''
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = User.objects.get(email=serializer.data['email'])
+        user.delete()
+        # import pdb; pdb.set_trace()
+        data = {
+            'user': UserModelSerializer(user).data
         }
         return Response(data, status=status.HTTP_200_OK)
