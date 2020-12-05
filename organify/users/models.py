bcf659a1ae465a1e4735b41031e92185d8d9703a
Xavier Medina <xmedinavei@gmail.com>
2020-12-05 09:06:06 -0500

    Adding new functions

--- organify/users/models.py
+++ organify/users/models.py
@@ -10,7 +10,6 @@ class User(AbstractUser):
     Extends from OrganifyBaseModel and Django AbstractUser.
     We add the email field here and put it as username field.
     '''
-    
     email = models.EmailField(unique=True,
                               error_messages={'unique': 'This email already exists.'})
     created = models.DateTimeField(auto_now_add=True,
@@ -19,24 +18,20 @@ class User(AbstractUser):
                                     help_text='Object datetime when modified.')
     is_admin = models.BooleanField(default=False,
                                    help_text='Superusers can change this to True by django admin.')
-
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     class Meta:
         '''Meta options.'''
-       
         get_latest_by = 'created'
         ordering = ['-created', '-modified']
 
     def __str__(self):
         '''Return id, email, first_name and last_name.'''
-
         to_show = f'userid {self.id}, {self.email}, {self.username}, {self.first_name} {self.last_name}'
         return to_show
 
     def get_short_name(self):
         '''Return the full name. Method in Abstract User.'''
-
         full_name = f'{self.first_name} {self.last_name}'
         return full_name
